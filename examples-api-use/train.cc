91f88c616f452cfa04984f1eecb5795f027723c2
David Fox <djwfox@gmail.com>
2025-02-19 12:52:27 +0000

    Try 3 lines

--- examples-api-use/train.cc
+++ examples-api-use/train.cc
@@ -156,7 +156,7 @@ int main(int argc, char *argv[]) {
     offscreen->Fill(bg_color.r, bg_color.g, bg_color.b);
 
     int line_offset = 0;
-    strncpy(text_buffer, "1st 14:03 London Liverpool Street", 256);
+    strncpy(text_buffer, "1st 14:03 Leeds       Exp 14:05", 256);
     if (outline_font) {
     rgb_matrix::DrawText(offscreen, *outline_font,
                             x - 1, y + font.baseline() + line_offset,
@@ -169,7 +169,20 @@ int main(int argc, char *argv[]) {
                         letter_spacing);
     line_offset += font.height() + line_spacing;
 
-    strncpy(text_buffer, "2nd 14:18 London Liverpool Street", 256);
+    strncpy(text_buffer, "This train is formed of 4 car...", 256);
+    if (outline_font) {
+    rgb_matrix::DrawText(offscreen, *outline_font,
+                            x - 1, y + font.baseline() + line_offset,
+                            outline_color, NULL, text_buffer,
+                            letter_spacing - 2);
+    }
+    rgb_matrix::DrawText(offscreen, font,
+                        x, y + font.baseline() + line_offset,
+                        color, NULL, text_buffer,
+                        letter_spacing);
+    line_offset += font.height() + line_spacing;
+
+    strncpy(text_buffer, "2nd 14:18 Leeds         On Time", 256);
     if (outline_font) {
     rgb_matrix::DrawText(offscreen, *outline_font,
                             x - 1, y + font.baseline() + line_offset,
